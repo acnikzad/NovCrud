f9f774274e1d1d3e54946bc54989c467b6c3772c
“Aria <a.c.nikzad@gmail.com>
2020-11-02 11:53:00 -0800

    added lot controller

--- app/controllers/api/lot_controller.rb
+++ app/controllers/api/lot_controller.rb
@@ -1,2 +1,44 @@
 class Api::LotController < ApplicationController
+
+  def index
+    @lots = Lot.all
+    render 'index.json.jb'
+  end
+
+  def show
+    the_id = params[:id]
+    @lot = Lot.find_by[id: the_id]
+    render 'show.json.jb'
+  end
+
+  def create
+    @lot = Lot.create(
+      name: params[:name],
+      location: params[:location],
+      phone: params[:phone]
+      )
+    @lot.save
+    render 'show.json.jb'
+  end
+
+  def update 
+    the_id = params[:id]
+    @lot = Lot.find_by(id: the_id)
+    @lot.name = params[:name] || @lot.name
+    @lot.location = params[:location] || @lot.location
+    @lot.phone = params[:phone] || @lot.phone
+
+    @lot.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @lot = Lot.find_by(id:params[:id])
+
+    @lot.destroy
+    @lot.save
+
+    render 'destroy.json.jb'
+  end
+
 end
